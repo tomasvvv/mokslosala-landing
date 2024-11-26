4edcbda34ef444ccd70adf656068b72a430d647d
Tomas Vosylius <tomas.v@atinvesticija.com>
2024-11-26 20:54:20 +0200

    options

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,7 @@
 const LINE_SELECTOR = ".running-line-text";
-const TEXT = " & mokslo sala > sveiki atvykę";
+const TEXT = "& mokslo sala > sveiki atvykę";
 const SPEED = 67;
-const INTERVAL = 100;
-
-// $(window).on("load", () => {
-//   const $line = $(LINE_SELECTOR);
-//   const initialText = $line.text();
-
-//   let tick = 0;
-
-//   setInterval(() => {
-//     console.log("tick", tick);
-
-//     const currentText = $line.text();
-//     if (tick >= initialText.length - 1) {
-//       tick = 0;
-//     }
-
-//     const newText = currentText.slice(1) + currentText[0];
-
-//     console.log("text: ", newText);
-
-//     $line.text(newText);
-//     tick++;
-//   }, 100);
-// });
+const INTERVAL = 125;
 
 $(window).on("load", () => {
   const $line = $(LINE_SELECTOR);
@@ -32,7 +9,7 @@ $(window).on("load", () => {
   const initialText = $line.text();
 
   const pushText = () => {
-    const newText = $line.text() + initialText;
+    const newText = $line.text() + " " + initialText;
     $line.text(newText);
   };
 
@@ -45,7 +22,6 @@ $(window).on("load", () => {
 
   setInterval(() => {
     const currentLeft = parseInt($line.css("left").split("px")[0]) || 0;
-    // const speed = 27.9; // parseInt($line.css("font-size").split("px")) || SPEED;
 
     oddEvenTracker = (oddEvenTracker + 1) % 2;
 
@@ -57,4 +33,98 @@ $(window).on("load", () => {
 
     $line.css("left", `${newLeft}px`);
   }, INTERVAL);
+});
+
+$(window).on("load", () => {
+  const $line = $(LINE_SELECTOR);
+  const screenWidth = $(window).width();
+  const initialText = $line.text();
+
+  const pushText = () => {
+    const newText = $line.text() + " " + initialText;
+    $line.text(newText);
+  };
+
+  while ($line.width() < screenWidth) {
+    pushText();
+  }
+  pushText();
+
+  let oddEvenTracker = 1;
+
+  setInterval(() => {
+    const currentLeft = parseInt($line.css("left").split("px")[0]) || 0;
+
+    oddEvenTracker = (oddEvenTracker + 1) % 2;
+
+    const newLeft = currentLeft - (oddEvenTracker ? 27.9 : 28.0);
+
+    if ($line.width() + newLeft < screenWidth) {
+      pushText();
+    }
+
+    $line.css("left", `${newLeft}px`);
+  }, INTERVAL);
+});
+
+$(window).on("load", () => {
+  const $line = $(".running-line2-text");
+  const screenWidth = $(window).width();
+  const initialText = $line.text();
+
+  const pushText = () => {
+    const newText = $line.text() + " " + initialText;
+    $line.text(newText);
+  };
+
+  while ($line.width() < screenWidth) {
+    pushText();
+  }
+  pushText();
+
+  let oddEvenTracker = 1;
+
+  setInterval(() => {
+    const currentLeft = parseInt($line.css("left").split("px")[0]) || 0;
+
+    oddEvenTracker = (oddEvenTracker + 1) % 2;
+
+    const newLeft = currentLeft - (oddEvenTracker ? 137.9 : 138.0);
+
+    if ($line.width() + newLeft < screenWidth) {
+      pushText();
+    }
+
+    $line.css("left", `${newLeft}px`);
+  }, INTERVAL);
+});
+
+$(window).on("load", () => {
+  const $line = $(".running-line3-text");
+  const screenWidth = $(window).width();
+  const initialText = $line.text();
+
+  const pushText = () => {
+    const newText = $line.text() + " " + initialText;
+    $line.text(newText);
+  };
+
+  while ($line.width() < screenWidth) {
+    pushText();
+  }
+  pushText();
+
+  let oddEvenTracker = 1;
+
+  setInterval(() => {
+    const currentLeft = parseInt($line.css("left").split("px")[0]) || 0;
+
+    const newLeft = currentLeft - 1;
+
+    if ($line.width() + newLeft < screenWidth) {
+      pushText();
+    }
+
+    $line.css("left", `${newLeft}px`);
+  }, 10);
 });
