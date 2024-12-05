a8f6bf327428be20ceecabc78d0ccf22e174ae72
Tomas Vosylius <tomas.v@atinvesticija.com>
2024-12-05 18:49:36 +0200

    test

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,12 @@
 $(function () {
+  let idx = 0;
+  // const colArray = ["#ff0000", "#ff00ff", "#00ff00"];
+
+  // $(window).on("resize", () => {
+  //   $("#mobile-navigation").css("background-color", colArray[idx]);
+  //   idx = (idx + 1) % 3;
+  // });
+
   const handleOpen = () => {
     $("html").css("overflow", "hidden");
     $("body").css("overflow", "hidden");
