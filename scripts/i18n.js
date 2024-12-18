61d5265cb6ed5795e5a45b68bf1b2fad775e60e9
Tomas Vosylius <tomas.v@atinvesticija.com>
2024-12-18 17:30:03 +0200

    changes

--- scripts/i18n.js
+++ scripts/i18n.js
@@ -2,41 +2,49 @@ const TRANSLATIONS = {
   "button-tickets": ["Bilietai", "Tickets"],
   "button-contacts": ["Kontaktai", "Contacts"],
   "heading-1": ["O jei žinotume daugiau?", "What if we knew more?"],
-  "working-hours": ["<b>Darbo laikas:</b><br/>Kasdien 10:00-19:00<br/>Nedirbame pirmadieniais.", "<b>Opening hours:</b><br/>Daily: 10:00-19:00</br>Closed on Mondays."],
-  "december-body": ["<b>Gruodžio mėnesį dirbame:</b><br/>14-15, 17-22, 28-29 dienomis.", "<b>In December, we are open on:</b><br/>the 14th–15th, 17th–22nd, and 28th–29th."],
+  "working-hours": ["<b>Darbo laikas</b><br/>Kasdien 10:00-19:00<br/>Nedirbame pirmadieniais", "<b>Opening hours</b><br/>Daily: 10:00-19:00</br>Closed on Mondays"],
+  "december-body": [
+    "<b>Gruodžio mėnesį dirbame</b><br/>13 d. 16 val. - ATIDARYMO ŠVENTĖ<br/>14-15, 17-22, 27-29 dienomis",
+    "<b>December 13th 4 pm - OPENING CELEBRATION</b><br/>We work on 14-15, 17-22, 27-29 December",
+  ],
   "intro-text": [
-    "Mokslo sala – tai pirmas mokslo ir inovacijų populiarinimo centras Lietuvoje. Čia lankytojų laukia net 140 objektų talpinanti ilgalaikė ekspozicija, modernus planetariumas, besikuriančios STEAM laboratorijos, įvairios kultūrinės ir edukacinės veiklos.",
+    "Mokslo sala - tai pirmas mokslo ir inovacijų populiarinimo centras Lietuvoje. Čia lankytojų laukia net 140 objektų talpinanti ilgalaikė ekspozicija, modernus planetariumas, besikuriančios STEAM laboratorijos, įvairios kultūrinės ir edukacinės veiklos.",
     "Science Island is Lithuania's first Science and Innovation Promotion Centre. With a permanent exhibition of 140 interactive installations, a state-of-the-art planetarium, emerging STEAM laboratories, and various cultural and educational activities.",
   ],
   "for-visitors-title": ["Lankytojams:", "Visitor information:"],
   "for-visitors-body": [
     `<b>Darbo laikas</b><br />
           Kasdien 10:00-19:00<br />
-          Nedirbame pirmadieniais.<br />
+          Nedirbame pirmadieniais<br />
           <br />
-          <b>Mokslo salos administracija</b><br />
+          <b>Gruodžio mėnesį dirbame</b><br/>
+          13 d. 16 val. - ATIDARYMO ŠVENTĖ<br/>
+          14-15, 17-22, 27-29 dienomis <br/>
+          <br/>
+          <b>Administracija</b><br />
           I-V 09:00-18:00<br />
-          <a href="mailto:info@mokslosala.lt">info@mokslosala.lt</a><br />
+          <a href="mailto:info@mokslosala.lt" style="text-decoration:underline">info@mokslosala.lt</a><br />
           <br />
           Laukiame visų - Karaliaus Mindaugo pr. 50, Kaunas, 44307.
           <br/>
           <br />
-          Mokslo salos erdvės pritaikytos žmonėms su negalia: įrengti liftai, specialūs tualetai, patogios ekspozicijų salės.`,
+          Mokslo salos erdvės pritaikytos visiems: įrengti liftai, specialūs tualetai, patogios ekspozicijų salės.`,
     `
-        <b>Opening hours</b><br />
+        <b>Opening times</b><br />
           Daily: 10:00-19:00<br />
-          Closed on Mondays.<br />
+          Closed on Mondays<br />
+          <br/>
+          December 13th 4 pm - OPENING CELEBRATION<br/>
+          We work on 14-15, 17-22, 27-29 December <br/>
           <br />
-          <b>Science Island Administration</b><br />
+          <b>Administration</b><br />
           Monday to Friday: 09:00-18:00<br />
-          <a href="mailto:info@mokslosala.lt">info@mokslosala.lt</a><br />
+          <a href="mailto:info@mokslosala.lt" style="text-decoration:underline">info@mokslosala.lt</a><br />
           <br />
-          We welcome everyone at:<br/>
-          Karaliaus Mindaugo prospektas 50, Kaunas, 44307
+          We welcome everyone at: Karaliaus Mindaugo prospektas 50, Kaunas, 44307.
           <br/>
           <br />
-          Science Island spaces are fully accessible for visitors with disabilities, featuring elevators, specially designed restrooms, and accessible exhibition space.
-    
+          Science Island spaces are fully accessible for all visitors, featuring elevators, specially designed restrooms, and accessible exhibition space.
     `,
   ],
   "visit-us": ["Aplankykite dabar!", "Visit us now!"],
@@ -47,7 +55,7 @@ $(function () {
   let language = window.localStorage.getItem("ms_temp_landing_lang") || "LT";
 
   const setImgSrc = () => {
-    $(".entrance-image").attr("src", `assets/entrance_${language.toLowerCase()}.png`);
+    $(".entrance-image").attr("src", `assets/entrance_${language.toLowerCase()}.jpg`);
   };
 
   const translate = () => {
